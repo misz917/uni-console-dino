382452c10be2fe7e34296e71bfe07e750479eae6
User <m.szydlowski2003@gmail.com>
2024-10-21 16:52:43 +0200

    window resolution chosen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+pub struct XY<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> XY<T> {
+    pub const fn new(x: T, y: T) -> Self {
+        XY { x, y }
+    }
+}
+
+pub const WINDOW_RESOLUTION: XY<usize> = XY::new(80, 45);
+
+pub struct ScreenBuffer {
+    pub active_frame: Vec<Vec<char>>,
+    pub following_frame: Vec<Vec<char>>,
+}
+
+pub struct SpriteDrawer;
+
+impl SpriteDrawer {}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world!");
 }
