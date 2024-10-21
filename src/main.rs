d21f80e40b9268701686726308550de4b0ab6c47
User <m.szydlowski2003@gmail.com>
2024-10-21 18:42:36 +0200

    overcomplicated maybe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 pub struct XY<T> {
     pub x: T,
     pub y: T,
@@ -11,14 +13,28 @@ impl<T> XY<T> {
 
 pub const WINDOW_RESOLUTION: XY<usize> = XY::new(80, 45);
 
+struct Bitmap<T> {
+    resolution: XY<usize>,
+    map: Vec<Vec<T>>,
+}
+
+impl<T: std::clone::Clone> Bitmap<T> {
+    fn new(resolution: XY<usize>, default_contents: T) -> Self {
+        Bitmap {
+            resolution: resolution.clone(),
+            map: vec![vec![default_contents; resolution.x]; resolution.y],
+        }
+    }
+}
+
 pub struct ScreenBuffer {
     pub active_frame: Vec<Vec<char>>,
     pub following_frame: Vec<Vec<char>>,
 }
 
-pub struct SpriteDrawer;
+pub struct AssetServer;
 
-impl SpriteDrawer {}
+pub struct BitmapRenderer;
 
 fn main() {
     println!("Hello world!");
