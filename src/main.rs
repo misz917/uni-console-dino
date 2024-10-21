1fec8246d23eba061e313f62a2841ed345c2abfd
mil <m.szydlowski2003@gmail.com>
2024-10-21 20:38:19 +0200

    various changes

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
-use std::vec;
-
-pub struct XY<T> {
-    pub x: T,
-    pub y: T,
+#[derive(Clone, Copy, Debug)]
+struct XY<T> {
+    x: T,
+    y: T,
 }
 
 impl<T> XY<T> {
-    pub const fn new(x: T, y: T) -> Self {
+    const fn new(x: T, y: T) -> Self {
         XY { x, y }
     }
 }
 
-pub const WINDOW_RESOLUTION: XY<usize> = XY::new(80, 45);
+const WINDOW_RESOLUTION: XY<usize> = XY::new(80, 45);
 
 struct Bitmap<T> {
     resolution: XY<usize>,
     map: Vec<Vec<T>>,
 }
 
-impl<T: std::clone::Clone> Bitmap<T> {
+impl<T: Clone> Bitmap<T> {
     fn new(resolution: XY<usize>, default_contents: T) -> Self {
         Bitmap {
-            resolution: resolution.clone(),
-            map: vec![vec![default_contents; resolution.x]; resolution.y],
+            resolution: resolution,
+            map: vec![vec![default_contents.clone(); resolution.x]; resolution.y],
         }
     }
 }
 
-pub struct ScreenBuffer {
-    pub active_frame: Vec<Vec<char>>,
-    pub following_frame: Vec<Vec<char>>,
+struct ScreenBuffer {
+    active_frame: Vec<Vec<char>>,
+    following_frame: Vec<Vec<char>>,
 }
 
-pub struct AssetServer;
+struct AssetServer;
 
-pub struct BitmapRenderer;
+struct BitmapRenderer;
 
 fn main() {
     println!("Hello world!");
