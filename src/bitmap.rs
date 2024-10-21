2818377be99c4aa4b22abf3371f43dd2a0950ac1
mil <m.szydlowski2003@gmail.com>
2024-10-21 23:26:45 +0200

    bitmap stuff moved to separate file

---
+++ src/bitmap.rs
@@ -0,0 +1,29 @@
+pub use crate::utils::XY;
+
+pub struct Bitmap<T> {
+    pub resolution: XY<usize>,
+    pub map: Vec<Vec<T>>,
+}
+
+impl<T: Clone> Bitmap<T> {
+    pub fn new(resolution: XY<usize>, default_contents: T) -> Self {
+        Bitmap {
+            resolution: resolution,
+            map: vec![vec![default_contents.clone(); resolution.x]; resolution.y],
+        }
+    }
+}
+
+pub struct BitmapRenderer;
+impl BitmapRenderer {
+    pub fn print_bitmap(bitmap: &Bitmap<char>) {
+        for y in 0..bitmap.resolution.y {
+            for x in 0..bitmap.resolution.x {
+                print!("{}", bitmap.map[y][x]);
+            }
+            if y + 1 != bitmap.resolution.y {
+                print!("\n");
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
 use std::env;
+
 pub mod window;
 pub mod utils;
-pub use crate::utils::XY;
+pub mod bitmap;
+
+pub use crate::{utils::XY, bitmap::{Bitmap, BitmapRenderer}};
 
 const WINDOW_RESOLUTION: XY<usize> = XY::new(160, 45);
 //160x90 but x axis is 2 times denser
-
-struct Bitmap<T> {
-    resolution: XY<usize>,
-    map: Vec<Vec<T>>,
-}
-
-impl<T: Clone> Bitmap<T> {
-    fn new(resolution: XY<usize>, default_contents: T) -> Self {
-        Bitmap {
-            resolution: resolution,
-            map: vec![vec![default_contents.clone(); resolution.x]; resolution.y],
-        }
-    }
-}
 
 struct BitmapBuffer {
     active_frame: Bitmap<char>,
@@ -26,20 +15,6 @@ struct BitmapBuffer {
 }
 
 struct AssetServer;
-
-struct BitmapRenderer;
-impl BitmapRenderer {
-    fn print_bitmap(bitmap: &Bitmap<char>) {
-        for y in 0..bitmap.resolution.y {
-            for x in 0..bitmap.resolution.x {
-                print!("{}", bitmap.map[y][x]);
-            }
-            if y + 1 != bitmap.resolution.y {
-                print!("\n");
-            }
-        }
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
