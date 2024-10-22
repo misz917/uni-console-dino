fd2e7f9ab2e8fa522ae57890ece0068d439790ad
mil <m.szydlowski2003@gmail.com>
2024-10-22 16:58:02 +0200

    main loop upgraded

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::env;
+use core::time;
+use std::{env, process::exit, thread::sleep, time::{Duration, SystemTime}};
 
 pub mod asset_server;
 pub mod bitmap;
@@ -12,6 +13,7 @@ pub use crate::{
 
 const WINDOW_BORDER_WIDTH: usize = 1;
 const WINDOW_RESOLUTION: XY<usize> = XY::new(160, 40);
+const FPS_LIMIT: f32 = 6.0;
 
 fn prepare() {
     print!("{}[H", ANSI); // move to 0,0
@@ -21,7 +23,7 @@ fn prepare() {
     print!("{}[?25l", ANSI); // make cursor invisible
 }
 
-fn main() {
+fn separate_window_creation() {
     let args: Vec<String> = env::args().collect();
     let mut ready: bool = false;
     for arg in &args {
@@ -29,19 +31,31 @@ fn main() {
             ready = true;
         }
     }
-
     if !ready {
         window::WindowCreator::open_new_window(window::GnomeTerminal, WINDOW_RESOLUTION, WINDOW_BORDER_WIDTH);
-        return;
+        exit(0);
     }
+}
 
-    // ----Startup----
-    // prepare();
+fn main() {
+    separate_window_creation();
     let bitmap = Bitmap::new(WINDOW_RESOLUTION, '#');
     BitmapRenderer::print_bitmap(&bitmap, 1);
 
-    // ----Main loop----
+
+    let sleep_duration = 1.0 / FPS_LIMIT;
+    let mut i = 0;
     loop {
-        
+        let time = SystemTime::now();
+
+        // loop contents
+        println!("{} ", i);
+        i+=1;
+
+        if let Ok(elapsed) = time.elapsed() {
+            sleep(Duration::from_secs_f32(sleep_duration) - elapsed);
+        } else {
+            return;
+        }
     }
 }
