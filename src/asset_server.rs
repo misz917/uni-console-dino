f712b7c5e36a96d6dc22ee32114812fcb41fc3b0
User <m.szydlowski2003@gmail.com>
2024-10-23 19:36:21 +0200

    asset server parsers in progress

--- src/asset_server.rs
+++ src/asset_server.rs
@@ -1,7 +1,10 @@
+use std::{collections::HashMap, fs, process::exit};
 
-use std::collections::HashMap;
-
-use crate::utils::Sprite;
+use crate::{
+    bitmap::Bitmap,
+    utils::{Sprite, XY},
+    WINDOW_RESOLUTION,
+};
 
 pub struct AssetServer {
     assets: HashMap<String, Sprite>,
@@ -9,7 +12,7 @@ pub struct AssetServer {
 impl AssetServer {
     pub fn new() -> Self {
         AssetServer {
-            assets: HashMap::new()
+            assets: HashMap::new(),
         }
     }
 
@@ -18,5 +21,44 @@ impl AssetServer {
             Some(sprite) => return sprite,
             None => todo!(),
         }
+    }
+}
+
+pub struct SpriteFileReader;
+impl SpriteFileReader {
+    pub fn read(file_path: &str) -> &Bitmap<char> {
+        let contents = fs::read_to_string(file_path).expect("Could not read file");
+
+        // let resolution = Self::parse_header(parts[0]);
+        // let header: String = split_contents.split('x').collect();
+        // let dimensions: XY<usize> = XY::new(header, y)
+        // todo!()
+        // Self::parse_file_contents(&contents)
+        let mut output_bitmap: Bitmap<char>;
+        // Self::parse_file_contents(&contents, &mut output_bitmap);
+        todo!()
+    }
+
+    fn parse_file_contents(contents: &String, output_bitmap: &mut Bitmap<char>) {
+        let parts: Vec<&str> = contents.split(';').collect();
+        if parts.len() != 2 {
+            exit(2);
+        }
+        let resolution = Self::parse_header(parts[0]);
+        Self::parse_tail(parts[1], &resolution, output_bitmap);
+    }
+
+    fn parse_header(header: &str) -> XY<usize> {
+        let numbers: Vec<&str> = header.split('x').collect();
+        XY {
+            x: numbers[0].parse::<usize>().unwrap(),
+            y: numbers[0].parse::<usize>().unwrap(),
+        }
+    }
+
+    // returns an incomplete bitmap (without resolution)
+    fn parse_tail(tail: &str, resolution: &XY<usize>, output_bitmap: &mut Bitmap<char>) {
+        for i in 0..tail.len() {}
+        todo!()
     }
 }
