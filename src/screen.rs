ed7ef84761491352e07a56e134dd50896afd2fdf
mil <m.szydlowski2003@gmail.com>
2024-10-23 13:48:59 +0200

    code cleanup

--- src/screen.rs
+++ src/screen.rs
@@ -16,23 +16,14 @@ impl TerminalScreen {
         }
     }
     pub fn print_screen(&self) {
-        // Self::clear_screen();
         Self::move_cursor_home();
         BitmapRenderer::print_bitmap(&self.bitmap_buffer.active_frame, &self.border_width);
         Self::flush_terminal_buffer();
     }
 
-    fn clear_screen() {
-        print!("{}[2J", ANSI);
-    }
-
     // fixes a problem that shouldn't exist
     fn flush_terminal_buffer() {
         io::stdout().flush().unwrap();
-    }
-
-    fn move_cursor(position: XY<usize>) {
-        // print!()
     }
 
     fn move_cursor_home() {
