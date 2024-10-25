4ec53d97d3764205e7afe51cf5d76f5003de8019
mil <m.szydlowski2003@gmail.com>
2024-10-25 23:35:13 +0200

    added a trait for bitmap buffer

--- src/bitmap_buffer.rs
+++ src/bitmap_buffer.rs
@@ -4,25 +4,43 @@ use crate::{
     utils::{ErrorDisplayer, XY},
 };
 
+pub trait BufferManager {
+    fn new_following_frame(&mut self, frame: &Bitmap<char>);
+    fn get_active_frame(&self) -> &Bitmap<char>;
+    fn update(&mut self);
+}
+
 #[derive(Clone)]
 pub struct BitmapBuffer {
     active_frame: Bitmap<char>,
     following_frame: Bitmap<char>,
-    possible_update: bool,
     resolution: XY<usize>,
+    possible_update: bool,
 }
 impl BitmapBuffer {
     pub fn new(default_frame: &Bitmap<char>) -> Self {
-        let resolution = default_frame.resolution;
         BitmapBuffer {
             active_frame: default_frame.clone(),
             following_frame: default_frame.clone(),
+            resolution: default_frame.resolution,
             possible_update: true,
-            resolution,
         }
     }
+}
+impl BufferManager for BitmapBuffer {
+    fn new_following_frame(&mut self, new_frame: &Bitmap<char>) {
+        if new_frame.resolution != self.resolution {
+            ErrorDisplayer::error("New frame has incorrect resolution");
+        }
+        self.following_frame = new_frame.clone();
+        self.possible_update = true;
+    }
 
-    pub fn update(&mut self) {
+    fn get_active_frame(&self) -> &Bitmap<char> {
+        &self.active_frame
+    }
+
+    fn update(&mut self) {
         if !self.possible_update {
             return;
         }
@@ -36,17 +54,5 @@ impl BitmapBuffer {
             }
         }
         self.possible_update = false;
-    }
-
-    pub fn new_following_frame(&mut self, new_frame: &Bitmap<char>) {
-        if new_frame.resolution != self.resolution {
-            ErrorDisplayer::error("New frame has incorrect resolution");
-        }
-        self.following_frame = new_frame.clone();
-        self.possible_update = true;
-    }
-
-    pub fn get_active_frame(&self) -> &Bitmap<char> {
-        &self.active_frame
     }
 }
