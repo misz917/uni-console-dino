d940d4c8199cd5c6271c73f934205152712b18bb
mil <m.szydlowski2003@gmail.com>
2024-10-27 02:00:19 +0200

    animation tests in progress

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,7 @@ fn main() {
     let mut asset_server = AssetServer::new("/home/firstuser/Codes/githubRepos/uni-console-dino/src/assets/");
     let dino = *asset_server.load("dino.txt");
     let vase = *asset_server.load("vase.txt");
+    let animation = *asset_server.load("test_animation.txt");
 
     let mut _frame_count: u128 = 0;
     loop {
@@ -51,6 +52,7 @@ fn main() {
         frame_assembler.insert(&Label::new(&_frame_count.to_string()), &XY::new(1, 1));
         frame_assembler.insert(&dino, &XY::new(3, 33));
         frame_assembler.insert(&vase, &XY::new(30, 34));
+        frame_assembler.insert(&animation, &XY::new(45, 34));
 
         // let input = gnome_window.read_key();
 
