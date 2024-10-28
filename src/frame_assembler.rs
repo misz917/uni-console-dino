63402062365bc407cb77b41dd7be6737b106fbeb
mil <m.szydlowski2003@gmail.com>
2024-10-28 01:33:43 +0100

    basic animation handler works

--- src/frame_assembler.rs
+++ src/frame_assembler.rs
@@ -14,8 +14,9 @@ impl FrameAssembler {
 
     // places a sprite on a bitmap by upper left corner of the sprite
     pub fn insert(&mut self, drawable_object: &DrawableObject, position: &XY<i32>) {
-        for row in 0..drawable_object.get_bitmap().resolution.x {
-            for col in 0..drawable_object.get_bitmap().resolution.y {
+        let bitmap = drawable_object.get_bitmap();
+        for row in 0..bitmap.resolution.x {
+            for col in 0..bitmap.resolution.y {
                 let target_x = position.x + row as i32;
                 let target_y = position.y + col as i32;
     
@@ -23,15 +24,16 @@ impl FrameAssembler {
                     && (target_x as usize) < self.frame.resolution.x
                     && (target_y as usize) < self.frame.resolution.y
                 {
-                    self.frame.matrix[target_y as usize][target_x as usize] = drawable_object.get_bitmap().matrix[col][row];
+                    self.frame.matrix[target_y as usize][target_x as usize] = bitmap.matrix[col][row];
                 }
             }
         }
     }
 
     pub fn insert_mut(&mut self, drawable_object: &mut DrawableObject, position: &XY<i32>) {
-        for row in 0..drawable_object.get_bitmap().resolution.x {
-            for col in 0..drawable_object.get_bitmap().resolution.y {
+        let bitmap = drawable_object.get_bitmap_mut();
+        for row in 0..bitmap.resolution.x {
+            for col in 0..bitmap.resolution.y {
                 let target_x = position.x + row as i32;
                 let target_y = position.y + col as i32;
     
@@ -39,7 +41,7 @@ impl FrameAssembler {
                     && (target_x as usize) < self.frame.resolution.x
                     && (target_y as usize) < self.frame.resolution.y
                 {
-                    self.frame.matrix[target_y as usize][target_x as usize] = drawable_object.get_bitmap_mut().matrix[col][row];
+                    self.frame.matrix[target_y as usize][target_x as usize] = bitmap.matrix[col][row];
                 }
             }
         }
