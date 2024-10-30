e3441d6802d801cecda1fbf90415baea95a16f99
User <m.szydlowski2003@gmail.com>
2024-10-30 20:19:11 +0100

    prototype of collision checking

--- src/collision_detector.rs
+++ src/collision_detector.rs
@@ -23,7 +23,33 @@ impl CollisionDetector {
         self.objects.insert(name.to_owned(), new_object);
     }
 
-    pub fn does_collide(name: &str) -> bool {
-        todo!()
+    pub fn does_collide(&self, name: &str) -> Option<bool> {
+        if let Some(compared_object) = self.objects.get(name) {
+            for object in &self.objects {
+                todo!()
+            }
+            todo!()
+        } else {
+            return None;
+        }
+    }
+
+    fn check_collision(
+        size_a: XY<usize>,
+        position_a: XY<i32>,
+        size_b: XY<usize>,
+        position_b: XY<i32>,
+    ) -> bool {
+        let a_left = position_a.x;
+        let a_right = position_a.x + size_a.x as i32;
+        let a_top = position_a.y;
+        let a_bottom = position_a.y + size_a.y as i32;
+
+        let b_left = position_b.x;
+        let b_right = position_b.x + size_b.x as i32;
+        let b_top = position_b.y;
+        let b_bottom = position_b.y + size_b.y as i32;
+
+        return !(a_left >= b_right || a_right <= b_left || a_top >= b_bottom || a_bottom <= b_top);
     }
 }
