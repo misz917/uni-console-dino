9fa7f001aa7a216ca81ff84a83fe01b9f4d86c9c
mil <m.szydlowski2003@gmail.com>
2024-11-01 01:38:55 +0100

    collision detector constructor added

--- src/collision_detector.rs
+++ src/collision_detector.rs
@@ -1,5 +1,5 @@
 use crate::utils::XY;
-use std::collections::{HashMap, LinkedList};
+use std::collections::HashMap;
 
 struct HitBox {
     size: XY<usize>,
@@ -18,6 +18,12 @@ pub struct CollisionDetector {
     objects: HashMap<String, HitBox>,
 }
 impl CollisionDetector {
+    pub fn new() -> Self {
+        CollisionDetector {
+            objects: HashMap::new(),
+        }
+    }
+
     pub fn insert(&mut self, size: &XY<usize>, position: &XY<i32>, name: &str) {
         let new_object = HitBox::new(size, position);
         self.objects.insert(name.to_owned(), new_object);
