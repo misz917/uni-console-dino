4f21d9938405940adf5096496bdd8eed85f0afc2
mil <m.szydlowski2003@gmail.com>
2024-11-15 11:14:23 +0100

    small changes

--- src/game_states/menu.rs
+++ src/game_states/menu.rs
@@ -26,23 +26,24 @@ impl GameState for Menu {
     }
 
     fn on_enter(&mut self, view: &mut View, _task_scheduler: &mut TaskScheduler) {
-        view.insert_object(
-            "press_to_play_label",
-            false,
-            DrawableObject::Label(Label::new("Press any button to start")),
-            XY::new(
-                (WINDOW_RESOLUTION.x / 2 - 12) as i32,
-                (WINDOW_RESOLUTION.y - 3) as i32,
-            ),
-            None,
-        );
         view.insert_asset(
             "title_sign",
             false,
             "welcome_screen.txt",
             XY::new(
                 (WINDOW_RESOLUTION.x / 2 - 34) as i32,
-                (WINDOW_RESOLUTION.y / 2 - 5) as i32,
+                (WINDOW_RESOLUTION.y / 2 - 12) as i32,
+            ),
+            None,
+        );
+        let text = "Press any button to start";
+        view.insert_object(
+            "press_to_play_label",
+            false,
+            DrawableObject::Label(Label::new(text)),
+            XY::new(
+                (WINDOW_RESOLUTION.x - text.len()) as i32 / 2,
+                (WINDOW_RESOLUTION.y - 3) as i32,
             ),
             None,
         );
