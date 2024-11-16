161ccb8bd5618f82841941ff2b85d137c44c0075
mil <m.szydlowski2003@gmail.com>
2024-11-16 14:36:48 +0100

    layers updated

--- src/game_states/layers.rs
+++ src/game_states/layers.rs
@@ -2,9 +2,9 @@
 pub const LAYER_PLAYER: i16 = 0;
 pub const LAYER_VASE: i16 = 0;
 pub const LAYER_BIRD: i16 = 0;
-pub const LAYER_CLOUD: i16 = 0;
-pub const LAYER_TREE: i16 = 0;
-pub const LAYER_GRASS: i16 = 0;
-pub const LAYER_SMOKE: i16 = 0;
-pub const LAYER_SUN: i16 = 0;
-pub const LAYER_GROUND: i16 = 0;
+pub const LAYER_CLOUD: i16 = 2;
+pub const LAYER_TREE: i16 = -2;
+pub const LAYER_GRASS: i16 = -1;
+pub const LAYER_SMOKE: i16 = 1;
+pub const LAYER_SUN: i16 = -10;
+pub const LAYER_GROUND: i16 = -10;
