06f1fbd3542cd53f2a5ea4045f347a72cdfd4bdc
mil <m.szydlowski2003@gmail.com>
2024-11-19 00:22:10 +0100

    bitmap_buffer tests added but one fails

--- src/bitmap_utils/bitmap_buffer.rs
+++ src/bitmap_utils/bitmap_buffer.rs
@@ -1,9 +1,8 @@
+use super::bitmap::Bitmap;
 use crate::{
     asset_server::TRANSPARENT_CHAR,
     utils::{ErrorDisplayer, XY},
 };
-
-use super::bitmap::Bitmap;
 
 #[derive(Clone)]
 pub struct BitmapBuffer {
@@ -53,5 +52,97 @@ impl BufferManager for BitmapBuffer {
             return Some(Box::new(differences));
         }
         return None;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_test_bitmap(content: char, width: usize, height: usize) -> Bitmap<char> {
+        Bitmap::new(
+            XY {
+                x: width,
+                y: height,
+            },
+            content,
+        )
+    }
+
+    #[test]
+    fn test_bitmap_buffer_creation() {
+        let first_frame = create_test_bitmap('A', 3, 3);
+        let buffer = BitmapBuffer::new(&first_frame);
+
+        assert_eq!(buffer.resolution.x, 3);
+        assert_eq!(buffer.resolution.y, 3);
+
+        assert_eq!(buffer.active_frame.matrix[0][0], 'A');
+    }
+
+    #[test]
+    fn test_insert_frame_with_correct_resolution() {
+        let first_frame = create_test_bitmap('A', 3, 3);
+        let mut buffer = BitmapBuffer::new(&first_frame);
+
+        let second_frame = create_test_bitmap('B', 3, 3);
+
+        buffer.insert_frame(Box::new(second_frame.clone()));
+
+        assert_eq!(buffer.active_frame.matrix[0][0], 'A');
+        assert_eq!(buffer.following_frame.as_ref().unwrap().matrix[0][0], 'B');
+    }
+
+    #[test]
+    fn test_get_frame_no_difference() {
+        let first_frame = create_test_bitmap('A', 3, 3);
+        let second_frame = create_test_bitmap('A', 3, 3);
+        let mut buffer = BitmapBuffer::new(&first_frame);
+
+        buffer.insert_frame(Box::new(second_frame));
+
+        let difference_frame = buffer.get_frame();
+
+        let difference_frame = difference_frame.unwrap();
+        for row in difference_frame.matrix {
+            for cell in row {
+                assert_eq!(cell, TRANSPARENT_CHAR);
+            }
+        }
+    }
+
+    #[test]
+    fn test_get_frame_with_differences() {
+        let first_frame = create_test_bitmap('A', 3, 3);
+        let second_frame = create_test_bitmap('B', 3, 3);
+        let mut buffer = BitmapBuffer::new(&first_frame);
+
+        // Insert the second frame into the buffer
+        buffer.insert_frame(Box::new(second_frame));
+
+        // Get the difference frame
+        let difference_frame = buffer.get_frame();
+
+        // Verify that the difference frame has the new character 'B' where there is a change
+        let difference_frame = difference_frame.unwrap();
+        assert_eq!(difference_frame.matrix[0][0], 'B');
+        assert_eq!(difference_frame.matrix[1][1], 'B');
+        assert_eq!(difference_frame.matrix[2][2], 'B');
+        // The rest should be TRANSPARENT_CHAR
+        assert_eq!(difference_frame.matrix[0][1], TRANSPARENT_CHAR);
+        assert_eq!(difference_frame.matrix[0][2], TRANSPARENT_CHAR);
+        assert_eq!(difference_frame.matrix[1][0], TRANSPARENT_CHAR);
+        assert_eq!(difference_frame.matrix[2][0], TRANSPARENT_CHAR);
+        assert_eq!(difference_frame.matrix[2][1], TRANSPARENT_CHAR);
+    }
+
+    #[test]
+    fn test_get_frame_no_following_frame() {
+        let first_frame = create_test_bitmap('A', 3, 3);
+        let mut buffer = BitmapBuffer::new(&first_frame);
+
+        let difference_frame = buffer.get_frame();
+
+        assert!(difference_frame.is_none());
     }
 }
